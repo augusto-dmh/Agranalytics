fddee090bee7ff1cf2333843f02c9468757e8fa4
= <augustodemeloh@protonmail.com>
2025-01-08 23:48:01 -0300

    feat: add Farmer controller/routes

---
+++ app/Http/Controllers/FarmerController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Farmer;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class FarmerController extends Controller
+{
+    public function index()
+    {
+        $farmers = Farmer::paginate(10);
+
+        return view('farmers.index', compact('farmers'));
+    }
+
+    public function create()
+    {
+        return view('farmers.create');
+    }
+
+    public function store(Request $request)
+    {
+        $farmer = Farmer::create($request->farmer);
+
+        return view('farmers.edit', compact('farmer'));
+    }
+
+    public function show(Farmer $farmer)
+    {
+        return view('farmers.show', compact('farmer'));
+    }
+
+    public function edit(Farmer $farmer)
+    {
+        return view('farmers.edit', compact('farmer'));
+    }
+
+    public function update(Request $request, Farmer $farmer)
+    {
+        $farmer->update(getUpdateData($request->farmer, $farmer, ['password']));
+
+        return view('farmers.edit', compact('farmer'));
+    }
+
+    public function destroy($farmerId)
+    {
+        DB::table('farmers')
+            ->where('id', $farmerId)
+            ->delete();
+
+        $farmers = Farmer::paginate(10);
+
+        return view('farmers.index', compact('farmers'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FarmerController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/farmers', [FarmerController::class, 'index'])->name('farmers.index');
+Route::get('/farmers/{farmer}', [FarmerController::class, 'show'])->name('farmers.show');
+Route::post('/farmers', [FarmerController::class, 'store'])->name('farmers.store');
+Route::patch('/farmers/{farmer}', [FarmerController::class, 'update'])->name('farmers.update');
+Route::delete('/farmers/{id}', [FarmerController::class, 'destroy'])->name('farmers.destroy');
+Route::get('/farmers/create', [FarmerController::class, 'create'])->name('farmers.create');
+Route::get('/farmers/{farmer}/edit', [FarmerController::class, 'edit'])->name('farmers.edit');
