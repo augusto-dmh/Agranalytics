f43a45949f1e9104739f46af0f6103f34918a9ab
= <augustodemeloh@protonmail.com>
2025-01-09 23:55:42 -0300

    fix: reorder route definitions to avoid conflicts
    considering that routing in laravel implies roughly said the reading of the 'web.php' file from top to bottom in search for a matching route, getting in '/farmers/create' would, if the 'show' route is mentioned before it - '/farmers/{farmer} -, result in the request being sent to not the intended route. To avoid that, the routes got reordered.

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,9 @@ Route::get('/', function () {
 });
 
 Route::get('/farmers', [FarmerController::class, 'index'])->name('farmers.index');
+Route::get('/farmers/create', [FarmerController::class, 'create'])->name('farmers.create');
+Route::get('/farmers/{farmer}/edit', [FarmerController::class, 'edit'])->name('farmers.edit');
 Route::get('/farmers/{farmer}', [FarmerController::class, 'show'])->name('farmers.show');
 Route::post('/farmers', [FarmerController::class, 'store'])->name('farmers.store');
 Route::put('/farmers/{farmer}', [FarmerController::class, 'update'])->name('farmers.update');
 Route::delete('/farmers/{id}', [FarmerController::class, 'destroy'])->name('farmers.destroy');
-Route::get('/farmers/create', [FarmerController::class, 'create'])->name('farmers.create');
-Route::get('/farmers/{farmer}/edit', [FarmerController::class, 'edit'])->name('farmers.edit');
