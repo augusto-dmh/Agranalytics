4ec2d749ecdc6962d18afd7d853f59aa78bc7555
= <augustodemeloh@protonmail.com>
2025-01-12 00:20:30 -0300

    feat: add fadeIn,fadeOut,zoomIn animations to tailwind config
    these custom animations are implemented in toast component.

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,30 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            keyframes: {
+                fadeIn: {
+                    '0%': { opacity: '0', transform: 'scale(0.8)' },
+                    '25%': { opacity: '0.25', transform: 'scale(0.85)' },
+                    '50%': { opacity: '0.5', transform: 'scale(0.9)' },
+                    '75%': { opacity: '0.75', transform: 'scale(0.95)' },
+                    '100%': { opacity: '1', transform: 'scale(1)' },
+                },
+                fadeOut: {
+                    '0%': { opacity: '1', transform: 'scale(1)' },
+                    '50%': { opacity: '0.5', transform: 'scale(1)' },
+                    '100%': { opacity: '0', transform: 'scale(0.95)' },
+                },
+                zoomIn: {
+                    '0%': { transform: 'scale(1)' },
+                    '50%': { transform: 'scale(1.1)' },
+                    '100%': { transform: 'scale(1.2)' },
+                },
+            },
+            animation: {
+                fadeIn: 'fadeIn var(--fadeIn-duration) ease-in forwards',
+                fadeOut: 'fadeOut var(--fadeOut-duration, 0.5s) ease-out forwards',
+                zoomIn: 'zoomIn 0.2s forwards'
+            },
         },
     },
     plugins: [],
